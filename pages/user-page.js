71072fd852fa4c402533e1b170b0c8afd1808011
Alice Wood <125405269+aliceswood@users.noreply.github.com>
2023-09-12 12:56:17 +0100

    image tags used to add profile photo

--- pages/user-page.js
+++ pages/user-page.js
@@ -1,12 +1,20 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function UserPage() {
   return (
     <>
       <h1>Welome to your User Page!</h1>
+      <Image
+        src="/images/profile.jpg" // Route of the image file
+        height={100} // Desired size with correct aspect ratio
+        width={80} // Desired size with correct aspect ratio
+        alt="Alice"
+      />
       <h2>
         <Link href="/">Back to Homepage</Link>
       </h2>
     </>
   );
 }
+
