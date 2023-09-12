9e645bafd048b2b506385bc0617abf79fdc8c2e7
Alice Wood <125405269+aliceswood@users.noreply.github.com>
2023-09-12 13:36:14 +0100

    adding head tags and titles to each page

--- pages/films/first-film.js
+++ pages/films/first-film.js
@@ -1,8 +1,12 @@
 import Link from 'next/link';
+import Head from 'next/head';
 
 export default function FirstFilm () {
   return (
     <>
+      <Head>
+        <title>First Film</title>
+      </Head>
       <h1>First Film!</h1>
       <h2>
         <Link href="/">Back to Homepage</Link>
--- pages/user-page.js
+++ pages/user-page.js
@@ -1,14 +1,18 @@
 import Link from 'next/link';
 import Image from 'next/image';
+import Head from 'next/head';
 
 export default function UserPage() {
   return (
     <>
+    <Head>
+      <title>User Page</title>
+    </Head>
       <h1>Welome to your User Page!</h1>
       <Image
-        src="/images/profile.jpg" // Route of the image file
-        height={100} // Desired size with correct aspect ratio
-        width={80} // Desired size with correct aspect ratio
+        src="/images/profile.jpg" 
+        height={100}
+        width={80}
         alt="Alice"
       />
       <h2>
