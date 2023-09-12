ce4be53b55e29f5fee4d0a8c7e81e1ddde407172
Alice Wood <125405269+aliceswood@users.noreply.github.com>
2023-09-12 14:19:49 +0100

    creating and importing global and module css files

--- pages/films/first-film.js
+++ pages/films/first-film.js
@@ -1,9 +1,11 @@
 import Link from 'next/link';
 import Head from 'next/head';
+import Layout from '../../components/layout';
+
 
 export default function FirstFilm () {
   return (
-    <>
+    <Layout>
       <Head>
         <title>First Film</title>
       </Head>
@@ -11,6 +13,6 @@ export default function FirstFilm () {
       <h2>
         <Link href="/">Back to Homepage</Link>
       </h2>
-    </>
+    </Layout>
   );
 }
