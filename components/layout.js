ee5cd37d237860c989bb68049ff4c6db685f251d
Alice Wood <125405269+aliceswood@users.noreply.github.com>
2023-09-13 14:56:39 +0100

    files added for dynamic routing practice

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
 const name = 'Alice';
-export const siteTitle = 'Next.js Sample Website';
+export const siteTitle = 'Romcom Rater';
 
 export default function Layout({ children, home }) {
   return (
