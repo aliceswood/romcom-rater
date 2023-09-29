e1fe0f814e0dc4369592b2ff7f6bae54479a41d7
Alice Wood <125405269+aliceswood@users.noreply.github.com>
2023-09-29 14:12:57 +0100

    updating user page

--- pages/user-page.js
+++ pages/user-page.js
@@ -1,23 +1,55 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import Head from 'next/head';
+import styles from '../styles/user-page.module.css';
+import utilStyles from '../styles/utils.module.css';
 
-export default function UserPage() {
+const  name = 'Alice'
+export default function UserPage({children, home}) {
   return (
     <>
     <Head>
-      <title>User Page</title>
+      <title>RomCom Rater: User Page</title>
     </Head>
-      <h1>Welome to your User Page!</h1>
-      <Image
-        src="/images/profile.jpg" 
-        height={100}
-        width={80}
-        alt="Alice"
-      />
-      <h2>
-        <Link href="/">Back to Homepage</Link>
-      </h2>
+    <header className={styles.header}>
+        {home ? (
+          <>
+            <Image
+              priority
+              src="/images/profile.jpg"
+              className={utilStyles.borderCircle}
+              height={144}
+              width={144}
+              alt=""
+            />
+            <h1 className={utilStyles.heading2Xl}>{name}</h1>
+            </>
+        ) : (
+          <>
+            <Link href="/">
+              <Image
+                priority
+                src="/images/profile.jpg"
+                className={utilStyles.borderCircle}
+                height={108}
+                width={108}
+                alt=""
+              />
+            </Link>
+            <h2 className={utilStyles.headingLg}>
+              <Link href="/" className={utilStyles.colorInherit}>
+                {name}
+              </Link>
+            </h2>
+          </>
+        )}
+      </header>
+      <main>{children}</main>
+      {!home && (
+        <div className={styles.backToHome}>
+          <Link href="/">← Back to home</Link>
+        </div>
+      )}
     </>
   );
 }
