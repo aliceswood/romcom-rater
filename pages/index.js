9c911707196de17d7c6fa6bad30408cfdb1e1f33
Alice Wood <125405269+aliceswood@users.noreply.github.com>
2023-09-29 14:12:29 +0100

    streamlining files - removed layout files

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,25 @@
 import Head from 'next/head';
-import Layout, { siteTitle } from '../components/layout';
+// import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-
-
+export const siteTitle = 'Romcom Rater';
 export default function Home() {
-  return (
-    <Layout home>
-      <Head>
+
+return (
+  <>
+    <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
         <p>
           (This page will be amended to be Romcom rater screen!)
         </p>
-        <h2>
+      </section>
+      <h2>
         <Link href="/user-page">User page</Link>
       </h2>
-      </section>
-    </Layout>
+  </>
+
   );
 }
