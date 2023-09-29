e36bb3e15746b1cd4286ef5f8b97af5a695f9e72
Alice Wood <125405269+aliceswood@users.noreply.github.com>
2023-09-29 12:41:12 +0100

    removing unnecessary files from blog post tutorial:

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import Link from 'next/link';
+
+
 
 export default function Home() {
   return (
@@ -12,6 +15,9 @@ export default function Home() {
         <p>
           (This page will be amended to be Romcom rater screen!)
         </p>
+        <h2>
+        <Link href="/user-page">User page</Link>
+      </h2>
       </section>
     </Layout>
   );
