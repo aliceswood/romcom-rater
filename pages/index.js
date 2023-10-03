52ff35edc83f8e497cf9e8953840c8559e693698
Alice Wood <125405269+aliceswood@users.noreply.github.com>
2023-10-03 12:38:42 +0100

    api call moved to home page and home test updated

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,39 @@
 import Head from 'next/head';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
-import Films from './films';
 
 export const siteTitle = 'Romcom Rater';
-export default function Home() {
+export async function getServerSideProps() {
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: `Bearer ${process.env.TMBD_API_KEY}`
+    }
+  };
+  const filteredUrl = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US%2Cen-UK&page=1&sort_by=popularity.desc&with_genres=10749%2C35&with_original_language=en"
+  const res = await fetch(filteredUrl, options)
+  const repo = await res.json()
+  return { props: { repo } }
+}
 
-return (
-  <>
-    <Head>
+export default function Home({repo}) {
+  console.log(repo)
+
+  const filmList = repo.results.map(((film, i) => (
+    <p key={i}>{film.title}</p>
+  )))
+  
+  return (
+    <>
+      <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={utilStyles.headingMd} data-testid="romComPlaceholder">
-        <div>
-          <Films/>
-        </div>
-      </section>
+      <div data-testid="filmTitle">{filmList}</div>
       <div className={utilStyles.navigationLinks}>
         <Link href="/user-page" data-testid="userPageLink" id="userLink">User page →</Link>
       </div>
-  </>
+    </>
+  )
 
-  );
-}
+  }
--- pages/index.test.js
+++ pages/index.test.js
@@ -2,10 +2,18 @@ import Home from "./index";
 import "@testing-library/jest-dom";
 import {render, screen} from "@testing-library/react"
 
+const filmOne = {title:'film-one'}
+const filmTwo = {title:'film-two'}
+const mockrepo = {results:[filmOne, filmTwo]}
+
 describe("Homepage", () => {
-  it("renders the homepage", () => {
-    render(<Home/>);
-    expect(screen.getByTestId("romComPlaceholder")).toBeInTheDocument();
+  it("renders the homepage", async () => {
+
+    await render(<Home repo={mockrepo}/>);
+    expect(screen.getByTestId("filmTitle")).toBeInTheDocument();
+    expect(screen.getByText('film-one'));
+    expect(screen.getByText('film-two'));
     expect(screen.getByTestId("userPageLink")).toBeInTheDocument();
   });
 });
+
