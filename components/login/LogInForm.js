90dab949a8379ee04941ccbf626f4063a52c48a8
Alice Wood <125405269+aliceswood@users.noreply.github.com>
2023-10-06 14:38:16 +0100

    log in form renders with correct fields

--- components/login/LogInForm.js
+++ components/login/LogInForm.js
@@ -6,10 +6,22 @@ export default function LogInForm ({}) {
     <> 
       <p>Log in here:</p>
       <form>   
-        <input placeholder="Email address" type="text"/>
-        <input placeholder="Username" type="text"/>
-        <input placeholder="Password" type="password"/>
-        <button type="submit">Log in!</button>
+        <input 
+        placeholder="Email address" 
+        type="text"
+        data-testid="emailField"
+        />
+        <input 
+        placeholder="Username" 
+        type="text"
+        data-testid="usernameField"
+        />
+        <input 
+        placeholder="Password" 
+        type="password"
+        data-testid="passwordField"
+        />
+        <button type="submit" data-testid="logInButton">Log in!</button>
       </form>
     </>
   )
---
+++ components/login/loginform.test.js
@@ -0,0 +1,15 @@
+import "@testing-library/jest-dom";
+import LogInForm from "./LogInForm";
+import {render, screen} from "@testing-library/react";
+
+jest.mock('next/router', () => jest.requireActual('next-router-mock'));
+
+describe("logInForm", () => {
+  it("renders the log in Form correctly", () => {
+    render(<LogInForm/>)
+    expect(screen.getByTestId("emailField")).toBeInTheDocument();
+    expect(screen.getByTestId("usernameField")).toBeInTheDocument();
+    expect(screen.getByTestId("passwordField")).toBeInTheDocument();
+    expect(screen.getByTestId("logInButton")).toBeInTheDocument();
+  });
+});
