b01239077bf39b5321ebe5024ae4b144d04628d9
Alice Wood <125405269+aliceswood@users.noreply.github.com>
2023-10-11 13:59:33 +0100

    updated supabase key and url variable names

--- middleware.js
+++ middleware.js
@@ -2,8 +2,8 @@ import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
 import { NextResponse } from 'next/server'
 
 export async function middleware(req) {
-  const supabaseUrl = process.env.SUPABASE_URL
-  const supabaseKey = process.env.SUPABASE_ANON_KEY
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+  const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
   const res = NextResponse.next()
   const supabase = createMiddlewareClient({ req, res }, {supabaseUrl, supabaseKey})
   await supabase.auth.getSession()
