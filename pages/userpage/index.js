87910f4612aad21e63c75713a3554d4b05e8440f
Alice Wood <125405269+aliceswood@users.noreply.github.com>
2024-05-20 18:09:42 +0100

    retrieve user from Supabase

--- pages/userpage/index.js
+++ pages/userpage/index.js
@@ -1,44 +1,80 @@
-import Link from 'next/link';
-import Head from 'next/head';
-import Navbar from '../../components/navbar/navbar';
-import Background from '../../components/background/Background';
-import { useRouter } from 'next/router'
+import Link from "next/link";
+import Head from "next/head";
+import Navbar from "../../components/navbar/navbar";
+import Background from "../../components/background/Background";
+import { supabase } from "../../lib/initSupabase";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 
+export default function UserPage() {
+	const [user, setUser] = useState(null);
+	const [name, setName] = useState("");
 
-const name = 'Alice'
-export default function UserPage({children, home}) {
+	const router = useRouter();
 
-  const handleSignOut = async (event) => {
-    event.preventDefault();
+	useEffect(() => {
+		async function fetchUser() {
+			const {
+				data: { session },
+			} = await supabase.auth.getSession();
 
+			if (session) {
+				try {
+					const { data: {user}, error } = await supabase.auth.getUser();
+					if (error) {
+						console.error("Error fetching user:", error.message);
+					} else {
+						setUser(user);
+						console.log("User fetched:", user);
+						setName(user.user_metadata.name);
+					}
+				} catch (err) {
+					console.error("Unexpected error fetching user:", err.message);
+				}
+			} else {
+				console.log("No active session found.");
+			}
+		}
+		fetchUser();
+	}, []);
 
-    await fetch('/api/auth/signout', {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      }
-    })
-    router.push('/login')
-  }
+	const handleSignOut = async (event) => {
+		event.preventDefault();
 
-  return (
-    <>
-      <Head>
-        <title>RomCom Rater: User Page</title>
-      </Head>
-        <>
-      <Background/>
-        <Navbar/>
-          <h2>
-              {name}
-          </h2>
-        </>
-      <div>
-        <Link href="/" data-testid="homeLink" id="homeLink">← Back to home</Link>
-      </div>
-      <button onClick={handleSignOut}>Sign out</button>
-    </>
-  );
+		await fetch("/api/auth/signout", {
+			method: "POST",
+			headers: {
+				Accept: "application/json",
+				"Content-Type": "application/json",
+			},
+		});
+		router.push("/login");
+	};
+
+	return (
+		<>
+			<Head>
+				<title>RomCom Rater: User Page</title>
+			</Head>
+			<>
+				<Background />
+				<Navbar />
+				<div>
+					{user ? (
+						<div key={user.id}>
+							<h2>{name}</h2>
+						</div>
+					) : (
+						<p>No user found.</p>
+					)}
+				</div>
+			</>
+			<div>
+				<Link href="/" data-testid="homeLink" id="homeLink">
+					← Back to home
+				</Link>
+			</div>
+			<button onClick={handleSignOut}>Sign out</button>
+		</>
+	);
 }
-
